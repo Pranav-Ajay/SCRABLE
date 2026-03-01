2c72cc059eacf5ae68d25a367f7fcc436966704d
Pranav-Ajay <pranavajay005@gmail.com>
2026-03-01 20:05:31 +0530

    Create map_check.js

---
+++ map_check.js
@@ -0,0 +1,50 @@
+async function validatePlaceInLocation(place, location) {
+
+  if (!place || !location) {
+    alert("Please enter both place and location");
+    return false;
+  }
+
+  const url =
+   `https://nominatim.openstreetmap.org/search?format=json&addressdetails=1&q=${encodeURIComponent(place)}`;
+
+  try {
+
+    const res = await fetch(url, {
+      headers: { "User-Agent": "FIR-System" }
+    });
+
+    const data = await res.json();
+
+    if (data.length === 0) {
+      alert("Place not found");
+      return false;
+    }
+
+    const address = data[0].address;
+
+    const hierarchy = [
+      address.city,
+      address.town,
+      address.village,
+      address.county,
+      address.state,
+      address.country
+    ].filter(Boolean).map(x => x.toLowerCase());
+
+    if (hierarchy.includes(location.toLowerCase())) {
+
+      return true;
+
+    } else {
+
+      alert("❌Place does not belong to that location");
+      return false;
+    }
+
+  } catch (error) {
+
+    alert("Error validating location");
+    return false;
+  }
+}
